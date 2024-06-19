504af93ee5604241a94e6fe1be84432b36f64f28
Natalí Trindade <salles.natali@gmail.com>
2024-06-19 16:08:37 -0300

    feat(component): add Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,3 @@
-import { ChangeEvent } from "react";
-import styles from "./Input.module.css";
-
 interface InputProps {
     label: string;
     type: "email" | "password";
@@ -8,39 +5,22 @@ interface InputProps {
     id: string;
     name: string;
     placeholder: string;
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    onBlur: () => void;
-    errorMessage: string;
     required?: boolean;
 }
 
-const Input = ({
-    label,
-    type,
-    value,
-    id,
-    name,
-    placeholder,
-    onChange,
-    onBlur,
-    errorMessage,
-    required = false
-}: InputProps) => {
+const Input = ({ label, type, value, id, name, placeholder, required = false }: InputProps) => {
+
     return (
-        <div className={styles.input_container}>
-            <label className={styles.label} htmlFor={id}>{label}</label>
+        <div >
+            <label htmlFor={id}>{label}</label>
             <input
-                className={styles.input}
                 value={value}
                 id={id}
                 name={name}
                 type={type}
                 placeholder={placeholder}
                 required={required}
-                onChange={onChange}
-                onBlur={onBlur}
             />
-            {errorMessage && <span className={styles.error}>{errorMessage}</span>}
         </div>
     )
 }
