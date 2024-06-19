d59f20b1d61301eec5c998285982569949e042cb
Natalí Trindade <salles.natali@gmail.com>
2024-06-19 16:27:56 -0300

    feat(component): add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+
+interface ButtonProps {
+    children: ReactElement | string;
+}
+
+const Button = (props: ButtonProps) => {
+    return (<button>
+        {props.children}
+    </button>)
+}
+
+export default Button;
