35e7dd95098f9563678942f3a76721007127b038
Natalí Trindade <salles.natali@gmail.com>
2024-06-19 16:43:05 -0300

    feat(actions): add actions for Form component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import Button from '../Button';
 import Input from '../Input';
 import styles from "./Form.module.css";
 
 const LoginForm = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        setEmail('');
+        setPassword('');
+    }
+
     return (
         <section className={styles.section}>
             <h1 className={styles.title}>Bem-vindo(a) à Taqtile!</h1>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={onFormSubmit}>
                 <Input
                     id='email'
                     label='Email'
                     name='email'
                     placeholder='Digite seu email'
                     type='email'
+                    value={email}
                     required={true}
+                    onChange={(value) => setEmail(value)}
                 />
                 <Input
                     id='password'
@@ -21,7 +34,9 @@ const LoginForm = () => {
                     name='password'
                     placeholder='Digite sua senha'
                     type='password'
+                    value={password}
                     required={true}
+                    onChange={(value) => setPassword(value)}
                 />
                 <Button>Entrar</Button>
             </form>
