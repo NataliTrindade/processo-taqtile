8729fc91e53112b7866a3f527a1276c28c5f024d
Natalí Trindade <salles.natali@gmail.com>
2024-06-21 00:17:00 -0300

    (hooks/useFormValidation): created custom hook useFormValidation for input validation

---
+++ src/hooks/useFormValidation.ts
@@ -0,0 +1,50 @@
+import { ChangeEvent, useState } from "react";
+import { validateEmail, validatePassword } from "../utils/inputValidations";
+
+interface UseFormValidationProps {
+    value: string;
+    setValue: React.Dispatch<React.SetStateAction<string>>;
+    onInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onBlur: () => void;
+    onFormSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+    errorMessage: string;
+}
+
+const useFormValidation = (inputType?: 'email' | 'password'): UseFormValidationProps => {
+    const [value, setValue] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const onInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setValue(event.target.value);
+    };
+
+    const onBlur = (): void => {
+        validateInput();
+    };
+
+    const onFormSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        setValue('');
+    };
+
+    const validateInput = (): void => {
+        let error = '';
+        if (inputType === "email") {
+            error = validateEmail(value);
+        } else if (inputType === "password") {
+            error = validatePassword(value);
+        }
+        setErrorMessage(error);
+    };
+
+    return {
+        value,
+        setValue,
+        onInputChange,
+        onBlur,
+        onFormSubmit,
+        errorMessage
+    };
+};
+
+export default useFormValidation;
