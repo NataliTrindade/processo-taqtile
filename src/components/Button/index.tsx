7b27c8d09ed515ab7079257e5c775f35fcd9879f
Natalí Trindade <salles.natali@gmail.com>
2024-06-23 19:39:42 -0300

    (components/Button): refactor Button component and add disabled attribute

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,15 +3,19 @@ import styles from "./Button.module.css";
 
 interface ButtonProps {
     children: ReactElement | string;
+    disabled: boolean;
 }
 
-const Button = (props: ButtonProps) => {
+const Button = ({ children, disabled }: ButtonProps) => {
     return (
         <button
-            type='submit'
-            className={styles.button}>
-            {props.children}
-        </button>)
+            type="submit"
+            disabled={disabled}
+            className={styles.button}
+        >
+            {children}
+        </button>
+    );
 }
 
 export default Button;
