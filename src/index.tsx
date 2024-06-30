b31f83eb505d05d2fe895dd0e40fc1412f27c3e3
Natalí Trindade <salles.natali@gmail.com>
2024-06-30 20:51:26 -0300

    add tailwind configuration files and import in required locations

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./styles.css"
 import { ApolloProvider } from '@apollo/client';
 import { client } from "./apollo/client";
+import "./styles/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
