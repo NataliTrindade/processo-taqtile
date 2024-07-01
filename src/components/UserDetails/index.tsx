8c3b954c2134353a22596298e17099a61e164c14
Natalí Trindade <salles.natali@gmail.com>
2024-06-30 21:01:25 -0300

    style UserDetails component

--- src/components/UserDetails/index.tsx
+++ src/components/UserDetails/index.tsx
@@ -1,30 +1,55 @@
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { GET_USER_DETAILS } from "../../graphql/queries/queries";
+import Button from "../Button";
+import Title from "../../Title";
+import { useNavigate } from 'react-router-dom';
 
 const UserDetails = () => {
     const { id } = useParams<{ id: string }>();
+    const navigate = useNavigate();
 
     const { loading, error, data } = useQuery(GET_USER_DETAILS, {
         variables: { id }
     });
 
-    if (loading) return <p>Carregando...</p>;
-    if (error) return <p>Erro: {error.message}</p>;
+    if (loading) return <p className="text-center my-40">Carregando...</p>;
+    if (error) return <p className="text-center my-40">{error.message}</p>;
     if (!data || !data.user) {
-        return <p>Dados do usuário não encontrados.</p>;
+        return <p className="text-center my-40">Dados do usuário não encontrados.</p>;
     }
 
     const { user } = data;
 
+    const navigateToUsers = () => navigate('/users');
+
     return (
-        <div>
-            <p>Data de Nascimento: {user.birthDate}</p>
-            <p>Email: {user.email}</p>
-            <p>Nome: {user.name}</p>
-            <p>Telefone: {user.phone}</p>
-            <p>Função: {user.role}</p>
-        </div>
+        <main className="main mx-0 min-h-screen">
+            <Title>Detalhes do Usuário</Title>
+            <div className="w-10/12 lg:w-2/4 mx-0">
+                <p className="user-item-details">
+                    <span className="font-bold mb-2 lg:mb-0">Data de Nascimento:</span>
+                    <span>{user.birthDate}</span>
+                </p>
+                <p className="user-item-details">
+                    <span className="font-bold mb-2 lg:mb-0">Email:</span>
+                    <span>{user.email}</span>
+                </p>
+                <p className="user-item-details">
+                    <span className="font-bold mb-2 lg:mb-0">Nome:</span>
+                    <span>{user.name}</span>
+                </p>
+                <p className="user-item-details">
+                    <span className="font-bold mb-2 lg:mb-0">Telefone:</span>
+                    <span>{user.phone}</span>
+                </p>
+                <p className="user-item-details mb-0">
+                    <span className="font-bold">Função:</span>
+                    <span>{user.role}</span>
+                </p>
+                <Button onClick={navigateToUsers}>Voltar</Button>
+            </div>
+        </main>
     );
 };
 
