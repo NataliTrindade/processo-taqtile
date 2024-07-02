5cfd26834eafd8bc801059cd6a725f9761768f51
Natalí Trindade <salles.natali@gmail.com>
2024-07-01 23:58:22 -0300

    refactor LoginForm to standardize form validation using the Formik and Yup libraries

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,80 +1,78 @@
-import { FormEvent } from 'react';
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
 import Button from '../Button';
-import Input from '../Input';
-import useFormValidation from '../../hooks/useFormValidation';
 import useAuthenticateUser from '../../auth/useAuthenticateUser';
 import { useNavigate } from 'react-router-dom';
 import Title from '../../Title';
 
+const initialValues = {
+    email: '',
+    password: ''
+}
+
+interface FormValues {
+    email: string,
+    password: string
+}
+const requiredMessage = 'O campo é obrigatório.';
+const emailMessage = 'Por favor, insira um endereço de e-mail válido.';
+const passwordMessage = 'Por favor, insira uma senha válida.';
+
+export const inputsLoginFormSchema = Yup.object().shape({
+    email: Yup.string()
+        .required(requiredMessage)
+        .email(emailMessage),
+
+    password: Yup.string()
+        .min(7, passwordMessage)
+        .required(requiredMessage)
+});
+
 const LoginForm = () => {
-    const {
-        value: email,
-        onInputChange: setEmail,
-        onBlur: validateEmailInput,
-        onFormSubmit: handleEmailSubmit,
-        errorMessage: errorMessageEmail
-    } = useFormValidation('email');
-
-    const {
-        value: password,
-        onInputChange: setPassword,
-        onBlur: validatePasswordInput,
-        onFormSubmit: handlePasswordSubmit,
-        errorMessage: errorMessagePassword
-    } = useFormValidation('password');
-
     const { authenticateUser, error, loading } = useAuthenticateUser();
     const navigate = useNavigate();
 
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        handleEmailSubmit(event);
-        handlePasswordSubmit(event);
-
-        if (errorMessageEmail || errorMessagePassword) {
-            return;
-        }
-
-        const authenticated = await authenticateUser(email, password);
+    const handleSubmit = async (values: FormValues, { setSubmitting }: FormikHelpers<FormValues>) => {
+        try{
+            const authenticated = await authenticateUser(values.email, values.password);
         if (authenticated === true) {
             navigate('/users');
         }
+        } catch (error) {
+            console.error(error);
+          } finally {
+            setSubmitting(false);
+          }
     };
 
     return (
-        <main className='main h-screen'>
+        <main className='custom-main'>
             <Title>Bem-vindo(a) à Taqtile!</Title>
-            <form className='form w-10/12 lg:w-2/4' method='post' onSubmit={handleSubmit}>
-                <Input
-                    id='email'
-                    label='Email'
-                    name='email'
-                    placeholder='Digite seu email'
-                    type='email'
-                    value={email}
-                    required={true}
-                    onChange={setEmail}
-                    onBlur={validateEmailInput}
-                    errorMessage={errorMessageEmail}
-                />
-                <Input
-                    id='password'
-                    label='Senha'
-                    name='password'
-                    placeholder='Digite sua senha'
-                    type='password'
-                    value={password}
-                    required={true}
-                    onChange={setPassword}
-                    onBlur={validatePasswordInput}
-                    errorMessage={errorMessagePassword}
-                />
-                <Button disabled={loading}>
-                    {loading ? "Estamos te conectando..." : 'Entrar'}
-                </Button>
-                {error && <p className='error text-center'>{error.message}</p>}
-            </form>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={inputsLoginFormSchema}
+                validateOnBlur={false}
+                validateOnChange={false}
+                onSubmit={handleSubmit}>
+                <>
+                    <Form className='custom-form'>
+                        <div>
+                            <label htmlFor="email" className='custom-label'>Email:</label>
+                            <Field type="text" id="email" name="email" className="custom-input" />
+                            <ErrorMessage name='email' render={error => <span className="custom-error inline-block">{error}</span>} />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className='custom-label'>Senha:</label>
+                            <Field type="password" id="password" name="password" className="custom-input" />
+                            <ErrorMessage name='password' render={msg => <span className="custom-error inline-block">{msg}</span>} />
+                        </div>
+                        <Button disabled={loading}>
+                            {loading ? "Estamos te conectando..." : 'Entrar'}
+                        </Button>
+                        {error && <p className='cutom-error text-center'>{error.message}</p>}
+                    </Form>
+                </>
+            </Formik>
         </main>
     );
 };
